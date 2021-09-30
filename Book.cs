e6dd59d354fce18447d7a567b6dc1e2ab49f9fc5
maronda <marondaanderson@gmail.com>
2021-09-29 21:01:58 -0400

    created add book method

--- Book.cs
+++ Book.cs
@@ -14,11 +14,16 @@ namespace Midterm
 
     public class Book : Media
     {
+        
 
         public string Title { get; set; }
         public string Author { get; set; }
         public string Status { get; set; }
         public DateTime DueDate { get; set; }
+
+
+        public string titleInput { get; set; }
+        public string authorName { get; set; }
 
 
 
@@ -33,72 +38,78 @@ namespace Midterm
 
 
         }
+        //Constructor added so AddBook Method works
+        public Book(string titleInput, string authorName)
+        {
+            this.titleInput = titleInput;
+            this.authorName = authorName;
+        }
     }
 
 
-        public class Library : Media
+    public class Library : Media
+    {
+
+        public static DateTime SetDueDate()
+        {
+            DateTime currentTime = DateTime.Now;
+            DateTime returnDate = currentTime.AddDays(14);
+
+            return returnDate;
+
+        }
+
+        public static List<Book> MakeBooks()
         {
 
-            public static DateTime SetDueDate()
-            {
-                DateTime currentTime = DateTime.Now;
-                DateTime returnDate = currentTime.AddDays(14);
-
-                return returnDate;
-
-             }
-
-            public static List<Book> MakeBooks()
-            {
-            
             const string f = @"../../../Books.txt";
-           
-            List<string> lines = new List<string>();
-                using (StreamReader r = new StreamReader(f))
-                {
-                    string line;
-                    while ((line = r.ReadLine()) != null)
-                    {
-                        lines.Add(line);
-                    }
-                }
-                Book book;
-                List<Book> library = new List<Book>();
-                foreach (string s in lines)
-                {
-                var split = s.Split('|');
-                    book = new Book(split[0], split[1], split[2], Convert.ToDateTime(split[3])); 
-                    library.Add(book);
 
+            List<string> lines = new List<string>();
+            using (StreamReader r = new StreamReader(f))
+            {
+                string line;
+                while ((line = r.ReadLine()) != null)
+                {
+                    lines.Add(line);
                 }
-                return library;
+            }
+            Book book;
+            List<Book> library = new List<Book>();
+            foreach (string s in lines)
+            {
+                var split = s.Split('|');
+                book = new Book(split[0], split[1], split[2], Convert.ToDateTime(split[3]));
+                library.Add(book);
 
             }
+            return library;
+
+        }
 
 
-         public static void SearchbyAuthor(List<Book> library)
+        public static void SearchbyAuthor(List<Book> library)
         {
             string userInput;
-            Console.WriteLine("Which author would you like to search for?:" );
+            Console.WriteLine("Which author would you like to search for?:");
 
             userInput = Console.ReadLine().ToLower();
 
             List<Book> matchedAuthor = new List<Book>();
-            
+
 
             for (int i = 0; i < library.Count; i++)
             {
-                if (library[i].Author.ToLower().Contains (userInput))
+                if (library[i].Author.ToLower().Contains(userInput))
                 {
                     matchedAuthor.Add(library[i]);
                 }
-                                   
+
             }
             if (matchedAuthor.Count < 1)
             {
 
                 Console.WriteLine("That search returned no results");
-                
+
             }
 
             foreach (Book b in matchedAuthor)
@@ -135,12 +146,12 @@ namespace Midterm
 
             foreach (Book b in matchedTitle)
             {
-                Console.WriteLine($"{b.Title, -40} {b.Author, -20}");
+                Console.WriteLine($"{b.Title,-40} {b.Author,-20}");
             }
         }
 
         public static void WriteToFile(List<Book> library)
-        {            
+        {
             const string f = @"../../../Books.txt";
             using (StreamWriter w = new StreamWriter(f))
             {
@@ -156,11 +167,11 @@ namespace Midterm
         public static void CheckOut(List<Book> library)
         {
             // Display book title with Number
-            for(int i = 0;  i < library.Count; i++)
+            for (int i = 0; i < library.Count; i++)
             {
                 Console.WriteLine($"[{i}]\t- {library[i].Title}");
-            }    
-            
+            }
+
             // Get user input
             Console.Write("Select the number of the book you would like to check out: ");
             string userInput = Console.ReadLine();
@@ -173,8 +184,8 @@ namespace Midterm
                 return;
             }
 
-            Book book = library[index];            
-            
+            Book book = library[index];
+
             // Check book's current status
             if (book.Status == "Checked Out")
             {
@@ -187,7 +198,7 @@ namespace Midterm
                 book.DueDate = SetDueDate();
                 Console.WriteLine($"{book.Title} is due back on {book.DueDate}.");
             }
-            
+
         }
 
         public static void ReturnBook(List<Book> library)
@@ -247,6 +258,31 @@ namespace Midterm
         }
 
 
+        public static void AddBook(List<Book> library)
+        {
+            string titleInput;
+            string authorName;
+            
+            Console.WriteLine("Please enter the name of book you would like to add to our Library");
+
+            titleInput = Console.ReadLine();
+
+            Console.WriteLine("Now enter the name of the author for the book you would like to add to our Library");
+
+            authorName = Console.ReadLine();
+
+
+
+            List<Book> addedBook = new List<Book>();
+
+            for (int i = 0; i < library.Count; i++)
+            {
+                
+
+                addedBook.Add(new Book(titleInput, authorName));
+            }
+
+            Console.WriteLine($"The book {titleInput} by {authorName} has been added to the library");
 
 
 
@@ -254,22 +290,18 @@ namespace Midterm
 
 
 
+            //Writes to book file
+            //const string k = @"../../../Books.txt";
+             //using (StreamWriter w = new StreamWriter(k))
+           // {
+           //     foreach (Book b in library)
+            //  {
+             //     w.WriteLine($"{b.Title}|{b.Author}|{b.Status}|{b.DueDate}");
+            //  }
+            // }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
     }
 }
